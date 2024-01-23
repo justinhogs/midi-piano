9ba4bc6e0d1c7d601829d432d259cef2e980ff19
justinhogs <76869663+justinhogs@users.noreply.github.com>
2024-01-24 00:14:55 +0800

    Change default octave

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+let currentOctave = 5
+
 const audioContext = new AudioContext()
 
 const NOTE_DETAILS = [
-  { note: "C", key: "Z", frequency: 261.626 },
-  { note: "Db", key: "S", frequency: 277.183 },
-  { note: "D", key: "X", frequency: 293.665 },
-  { note: "Eb", key: "D", frequency: 311.127 },
-  { note: "E", key: "C", frequency: 329.628 },
-  { note: "F", key: "V", frequency: 349.228 },
-  { note: "Gb", key: "G", frequency: 369.994 },
-  { note: "G", key: "B", frequency: 391.995 },
-  { note: "Ab", key: "H", frequency: 415.305 },
-  { note: "A", key: "N", frequency: 440 },
-  { note: "Bb", key: "J", frequency: 466.164 },
-  { note: "B", key: "M", frequency: 493.883 },
+  { note: "C", key: "Z", frequency: 32.703 },
+  { note: "Db", key: "S", frequency: 34.648 },
+  { note: "D", key: "X", frequency: 36.708 },
+  { note: "Eb", key: "D", frequency: 38.891 },
+  { note: "E", key: "C", frequency: 41.203 },
+  { note: "F", key: "V", frequency: 43.654 },
+  { note: "Gb", key: "G", frequency: 46.249 },
+  { note: "G", key: "B", frequency: 48.999 },
+  { note: "Ab", key: "H", frequency: 51.913 },
+  { note: "A", key: "N", frequency: 55 },
+  { note: "Bb", key: "J", frequency: 58.27 },
+  { note: "B", key: "M", frequency: 61.735 },
 ]
 
 document.addEventListener("keydown", (e) => {
@@ -23,6 +25,8 @@ document.addEventListener("keydown", (e) => {
   if (noteDetail == null) return
   noteDetail.active = true
   playNotes()
+
+  e.preventDefault()
 })
 
 document.addEventListener("keyup", (e) => {
@@ -32,20 +36,49 @@ document.addEventListener("keyup", (e) => {
   if (noteDetail == null) return
   noteDetail.active = false
   playNotes()
+  e.preventDefault()
 })
+
+document.addEventListener("keydown", (e) => {
+  const letter = e.code
+  const noteDetail = getNoteDetail(letter)
+
+  if (noteDetail == null) {
+    if (letter === "ArrowLeft") {
+      // Decrease the octave
+      changeOctave(-1)
+    } else if (letter === "ArrowRight") {
+      // Increase the octave
+      changeOctave(1)
+    }
+    return
+  }
+
+  noteDetail.active = true
+  playNotes()
+
+  e.preventDefault()
+})
+
+function changeOctave(direction) {
+  currentOctave = Math.max(Math.min(currentOctave + direction, 7), 1)
+  document.getElementById("current-octave").textContent = currentOctave
+}
 
 function getNoteDetail(keyboardKey) {
   return NOTE_DETAILS.find((n) => `Key${n.key}` === keyboardKey)
 }
 
 function startNote(noteDetail, gainValue) {
+  const octaveMultiplier = Math.pow(2, currentOctave - 4)
   const gainNode = audioContext.createGain()
   gainNode.gain.value = gainValue
   const oscillator = audioContext.createOscillator()
-  oscillator.frequency.value = noteDetail.frequency
-  oscillator.type = "sine"
+  oscillator.frequency.value = noteDetail.frequency * octaveMultiplier
+  oscillator.type = "square"
   oscillator.connect(gainNode).connect(audioContext.destination)
   oscillator.start()
+  console.log(octaveMultiplier)
   noteDetail.oscillator = oscillator
 }
 
@@ -59,8 +92,11 @@ function playNotes() {
     }
   })
   const activeNotes = NOTE_DETAILS.filter((n) => n.active)
+  console.log(activeNotes)
   const gain = 1 / activeNotes.length
   activeNotes.forEach((n) => {
     startNote(n, gain)
   })
 }
+
+// add arrow button to change octave left and right
